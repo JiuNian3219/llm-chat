eb412be0c527c801dd224129dec778f7463f6314
JiuNian <qin0yu0lou@gmail.com>
2025-05-23 23:40:07 +0800

    feat: 新增 Markdown 链接组件和Pre组件

---
+++ client/domain/chat/components/markdown/PreBlock/index.jsx
@@ -0,0 +1,11 @@
+/**
+ * Markdown Pre组件
+ * @param {object} props - 组件属性 
+ * @param {import("react").ReactNode} props.children - 组件子元素
+ * @returns 
+ */
+const PreBlock = ({ children }) => {
+  return children
+}
+
+export default PreBlock
