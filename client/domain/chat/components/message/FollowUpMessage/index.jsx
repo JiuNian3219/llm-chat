fe947be3fbb168dd9f2a2dd5fbd0f773c162d87e
JiuNian <qin0yu0lou@gmail.com>
2025-05-24 23:11:38 +0800

    feat: 前端优化FollowUpMessage组件，支持额外类名和样式

--- client/domain/chat/components/message/FollowUpMessage/index.jsx
+++ client/domain/chat/components/message/FollowUpMessage/index.jsx
@@ -1,8 +1,23 @@
 import { Button } from "antd";
 import styles from "./index.module.css";
 
-const FollowUpMessage = ({ message }) => {
-  return <Button className={styles.follow}>{message}</Button>;
+/**
+ * FollowUpMessage 组件
+ * @param {object} props - 组件属性
+ * @param {string} props.message - 展示信息
+ * @param {string} [props.className] - 额外的类名
+ * @param {React.CSSProperties} [props.style] - 额外的样式
+ * @returns
+ */
+const FollowUpMessage = ({ message, className, style }) => {
+  return (
+    <Button
+      className={`${styles.follow} ${className}`}
+      style={style}
+    >
+      {message}
+    </Button>
+  );
 };
 
 export default FollowUpMessage;
